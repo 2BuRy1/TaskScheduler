cb6bf02ccf4fb214035c94b13785f6732730f20d
2BuRy1 <kot514877@gmail.com>
2025-06-10 23:55:34 +0300

    some refactoring

--- scheduler/src/main/kotlin/org/kotlin/scheduler/configurations/BotInitializer.kt
+++ scheduler/src/main/kotlin/org/kotlin/scheduler/configurations/BotInitializer.kt
@@ -7,7 +7,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
 
 @Configuration
-class botInitializer {
+class BotInitializer {
 
 
 
